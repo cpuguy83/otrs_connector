a8b91c3c45c856f23d14eb652a1cd65119c80c8d
Brian Goff <cpuguy83@gmail.com>
2012-05-16 17:31:15 -0400

    Clean up/remove redundant items

--- lib/otrs_connector/otrs/change/work_order.rb
+++ lib/otrs_connector/otrs/change/work_order.rb
@@ -141,15 +141,7 @@ class OTRS::Change::WorkOrder < OTRS
       send("#{name.to_s.underscore.to_sym}=", value)
     end
   end
-  
-  def attributes
-    attributes = {}
-    self.instance_variables.each do |v|
-      attributes[v.to_s.gsub('@','').to_sym] = self.instance_variable_get(v)
-    end
-    attributes
-  end
-  
+
   def save
     self.create(self.attributes)
   end
