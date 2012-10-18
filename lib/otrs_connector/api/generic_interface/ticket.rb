cf2518d7bbc9a9baec6fd3c40bd30cb1fe8f2a0b
Brian Goff <cpuguy83@gmail.com>
2012-10-18 12:27:27 -0400

    Find method now correctly splits out DynamicFields and Artilces, creating new instances of each. Added save method to send new tickets to OTRS

--- lib/otrs_connector/api/generic_interface/ticket.rb
+++ lib/otrs_connector/api/generic_interface/ticket.rb
@@ -3,11 +3,17 @@ module OTRSConnector
     module GenericInterface
       class Ticket
         include OTRSConnector::API::GenericInterface
+        
+        class << self
+          # Create some extra instance class instance accessors for create/update methods
+          attr_accessor :default_history_type, :default_history_comment
+        end
         self.wsdl = OTRSConnector::API::GenericInterface.default_wsdl
         self.wsdl_endpoint = OTRSConnector::API::GenericInterface.default_wsdl_endpoint
         
         actions 'TicketGet', 'TicketCreate', 'TicketUpdate', 'TicketSearch'
         
+        attribute :id, type: Integer
         attribute :age, type: Integer
         attribute :archive_flag
         attribute :change_by, type: Integer
@@ -37,6 +43,7 @@ module OTRSConnector
         attribute :responsible_id, type: Integer
         attribute :slaid, type: Integer
         attribute :service_id, type: Integer
+        attribute :service
         attribute :solution_in_min, type: Integer
         attribute :solution_time
         attribute :ticket_number, type: Integer
@@ -47,9 +54,9 @@ module OTRSConnector
         attribute :until_time, type: Integer
         attribute :dynamic_fields
         attribute :articles
-        
-        after_initialize :setup_articles
-        after_initialize :setup_dynamic_fields
+        attribute :state_id, type: Integer
+        attribute :state
+        attribute :customer_user
         
         def self.find(id, options={})
           new_options = {}
@@ -72,36 +79,78 @@ module OTRSConnector
             end
           end
           new_options['TicketID'] = id
-          ticket = new( split_external_keys_from_ticket_hash self.connect('TicketGet', new_options)[:ticket] )
-          ticket.send do 'run_callbacks', :find
-            ticket
+          response = self.connect('TicketGet', new_options)[:ticket]
+          response[:id] = response[:ticket_id]
+          dynamic_fields = build_dynamic_fields_from_ticket_hash(response)
+          ticket = new response.except(:article)
+          ticket.articles = response[:article].collect do |a| 
+            a[:id] = a[:article_id]
+            Article.new a
           end
-        end
-        
-        def self.split_external_keys_from_ticket_hash(ticket)
-          dynamic_fields = {}
-          ticket.each do |key, value|
-            if key =~ /^dynamic_field_/
-              dynamic_fields[key] == value
-              ticket.delete(key)
-            elsif key =~ /^ticket_free_(key|text).*\d+/
-              ticket.delete(key)
-            end
-          end
-          ticket[:dynamic_fields] = dynamic_fields
-          ticket[:articles] = ticket[:article]
-          ticket.delete(:article)
+          ticket.dynamic_fields = dynamic_fields if dynamic_fields.any?
           ticket
         end
         
-        private
-          def setup_dynamic_fields
-            self.dynamic_fields = 'stuff'#self.dynamic_fields.collect { |f| DynamicField.new f }
+        # OTRS sends dynamic fields back as normal ticket fields, even though they should be separate
+        # Fields come back as DynamicField_X where X is the field name
+        # Here we split these out and create instances of the DynamicField class
+        def self.build_dynamic_fields_from_ticket_hash(ticket)
+          dynamic_fields = []
+          ticket.each do |key, value|
+            if key =~ /dynamic_field_/ and !value.nil?
+              dynamic_fields << { name: key.to_s.gsub('dynamic_field_', ''), value: value }
+            end
           end
+          dynamic_fields.collect{|f| DynamicField.new f}
+        end
+        
+        # Pass in an extra_options hash to supply your own history_type and history_comment
+        def save(extra_options={})
+          a = articles.first
           
-          def setup_articles
-            self.articles = 'more stuff'#self.articles.collect {|a| Article.new a }
-          end
+          # Get just the dynamic_field attributes to send to OTRS
+          new_dynamic_fields = dynamic_fields.collect{|f| {'Name' => f.name, 'Value' => f.value}} if dynamic_fields
+          options = {
+            'Ticket'          => {
+              'Title'         => title,
+              'QueueID'       => queue_id,
+              'Queue'         => queue,
+              'LockID'        => lock_id,
+              'Lock'          => lock,
+              'TypeID'        => type_id,
+              'Type'          => type,
+              'ServiceID'     => service_id,
+              'Service'       => service,
+              'SLAID'         => slaid,
+              'StateID'       => state_id,
+              'State'         => state,
+              'PriorityID'    => priority_id,
+              'Priority'      => priority,
+              'OwnerID'       => owner_id,
+              'Owner'         => owner,
+              'ResponsibleID' => responsible_id,
+              'Responsible'   => responsible,
+              'CustomerUser'  => customer_user
+            },
+            'Article' => {
+              'ArticleTypeID' => a.article_type_id,
+              'ArticleType'   => a.article_type,
+              'SenderType'    => a.sender_type,
+              'MimeType'      => a.mime_type || 'text/plain',
+              'Charset'       => a.charset || 'utf8',
+              'From'          => a.from,
+              'Subject'       => a.subject,
+              'Body'          => a.body,
+              'HistoryType'   => extra_options[:history_type] || self.class.default_history_type,
+              'HistoryComment'  => extra_options[:history_comment] || self.class.default_history_comment
+            },
+            'DynamicField' => new_dynamic_fields
+          }
+          response = self.class.connect 'TicketCreate', options
+          self.attributes = self.class.find(response[:ticket_id], dynamic_fields: true, articles: true).attributes
+          self
+        end
+        
         
       end
     end
