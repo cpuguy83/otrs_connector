b055a2233b39e4f02ece893ce90ccaddc7f9d1e1
Brian Goff <cpuguy83@gmail.com>
2012-10-19 14:47:24 -0400

    Class checker, used to see if Exception class exists in connect method

--- lib/otrs_connector.rb
+++ lib/otrs_connector.rb
@@ -9,6 +9,16 @@ module OTRSConnector
   def self.root
     File.expand_path('../..', __FILE__)
   end
+  
+  def self.class_exists?(class_name)
+    begin
+    klass = class_name.constantize
+    
+    rescue NameError
+      return false
+    end
+    true
+  end
 end
 
 require_relative 'otrs_connector/api'
